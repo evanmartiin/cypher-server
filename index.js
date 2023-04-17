80f3e38cbc6bc09f370b990d03b0eddb72dbf3ff
Evan Martin <evan.martiin29@gmail.com>
2023-04-17 15:36:53 +0200

    test: use chunks

--- index.js
+++ index.js
@@ -9,20 +9,34 @@ app.get("/", (req, res) => {
   res.send("<h1>Hello world</h1>");
 });
 
+const a =
+  "http://thinkingform.com/wp-content/uploads/2017/09/video-sample-mp4.mp4?_=1";
+const b =
+  "https://drive.google.com/file/d/1RCBMvLbERASjB9TNLISCrUdsj1oQi_t8/view";
+
 io.on("connection", (socket) => {
   console.log("a user connected");
-  
+
   socket.on('HELLO', (args) => {
     console.log(args)
   });
 
-  socket.on('CREATE_VIDEO', (args) => {
+  socket.on('CREATE_VIDEO', async (args) => {
     console.log('create video emit');
     io.emit('VIDEO_CREATED', args)
-
-    const buffers = Array.from({ length: 3 }, () => Buffer.from(args.buffer));
     
-    io.emit('VIDEO_MERGED', Buffer.concat(buffers));
+    const c = await fetch(a);
+    const d = await fetch(b);
+
+    const chunks = [];
+    for await (let chunk of c.body) {
+      chunks.push(chunk);
+    }
+    for await (let chunk of d.body) {
+      chunks.push(chunk);
+    }
+
+    io.emit('VIDEO_MERGED', Buffer.concat(chunks));
   });
 
 });
