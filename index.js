73601db3f3c0189a8ee76900bae17b24042cb7d1
Evan Martin <evan.martiin29@gmail.com>
2023-04-17 15:59:51 +0200

    c'est ciao

--- index.js
+++ index.js
@@ -9,11 +9,6 @@ app.get("/", (req, res) => {
   res.send("<h1>Hello world</h1>");
 });
 
-const a =
-  "http://thinkingform.com/wp-content/uploads/2017/09/video-sample-mp4.mp4?_=1";
-const b =
-  "https://drive.google.com/file/d/1RCBMvLbERASjB9TNLISCrUdsj1oQi_t8/view";
-
 io.on("connection", (socket) => {
   console.log("a user connected");
 
@@ -24,21 +19,7 @@ io.on("connection", (socket) => {
   socket.on('CREATE_VIDEO', async (args) => {
     console.log('create video emit');
     io.emit('VIDEO_CREATED', args)
-    
-    const c = await fetch(a);
-    const d = await fetch(b);
-
-    const chunks = [];
-    for await (let chunk of c.body) {
-      chunks.push(chunk);
-    }
-    for await (let chunk of d.body) {
-      chunks.push(chunk);
-    }
-
-    io.emit('VIDEO_MERGED', Buffer.concat(chunks));
   });
-
 });
 
 io.on("connect_error", (err) => {
