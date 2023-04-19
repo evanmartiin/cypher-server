ad4543c908f7329bf20d25e9faf1b3d6f8e0b8e9
justinqllvr <42967513+justinqllvr@users.noreply.github.com>
2023-04-19 16:31:15 +0200

    change the dropbow api call

--- DropBoxApi.js
+++ DropBoxApi.js
@@ -1,15 +1,14 @@
-
-
 class DropBoxApi {
   constructor() {
-    const Dropbox = require('dropbox').Dropbox;
-    console.log(process.env.VITE_DROPBOX_KEY)
+    const Dropbox = require("dropbox").Dropbox;
+    console.log(process.env.VITE_DROPBOX_KEY);
     const ACCESS_TOKEN = process.env.VITE_DROPBOX_KEY;
     this.dropbox = new Dropbox({ accessToken: ACCESS_TOKEN });
   }
 
   async getSingleVideo(path) {
-    console.log(path)
+    console.log(path);
+
     const res = this.dropbox
       .filesDownload({ path: path })
       .then((response) => {
